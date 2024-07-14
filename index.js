1db4c7a843511a1eb963b815eab9877ba63e54df
Pablo Moron <moronpablo94@gmail.com>
2024-07-13 23:48:55 -0300

    💡 add comments

--- index.js
+++ index.js
@@ -29,12 +29,17 @@ app.listen(3000, async () => {
     try {
         // mongoose connection string
         // [protocol]://[user]:[password]@[host]:[port]/[data_base]?[options_query_params]
-        await mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/pets?authSource=admin')
         /*
         For local MongoDB databases, we recommend using 127.0.0.1 instead of localhost.
         That is because Node.js 18 and up prefer IPv6 addresses, which means, on many machines, Node.js will resolve localhost to the IPv6 address ::1 and Mongoose will be unable to connect.
         Unless the mongodb instance is running with ipv6 enabled.
         */
+        // For localhost
+        await mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/pets?authSource=admin')
+
+        // For docker_example app container
+        // For docker_example build, switch connections
+        // await mongoose.connect('mongodb://admin:admin@mongo_container:27017/pets?authSource=admin')
 
         console.log('Server running on http://localhost:3000')
     } catch (error) {
